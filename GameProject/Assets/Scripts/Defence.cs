993af62b27012c8c68fc806b70b74be9418da112
Antol <antol.peshkov@gmail.com>
2014-02-08 18:28:01 +0400

    Подсветка частей тела при нажатии

---
+++ GameProject/Assets/Scripts/Defence.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defence : MonoBehaviour {
+
+	private GameObject _selectedBodyPart;
+	public GameObject selectedBodyPart {
+		set {
+			Debug.Log("triam");
+			this._selectedBodyPart = value;
+		}
+		get {
+			return this._selectedBodyPart;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GameProject/Assets/Scripts/PlayerBodyPart.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBodyPart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUpAsButton () {
+
+		GameObject player = GameObject.Find("Player");
+		Defence defence = player.GetComponent("Defence") as Defence;
+
+		GameObject selectedBodyPart = defence.selectedBodyPart;
+		if (selectedBodyPart) {
+			SpriteRenderer selectedRender = selectedBodyPart.GetComponent ("SpriteRenderer") as SpriteRenderer;
+			selectedRender.color = Color.white;
+		}
+
+		defence.selectedBodyPart = this.gameObject;
+
+		SpriteRenderer render = this.gameObject.GetComponent("SpriteRenderer") as SpriteRenderer;
+		render.color = Color.green;
+	}
+}
