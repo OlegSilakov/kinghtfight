ed6194f8e65c8249997525e63e5aed7a109a09da
Antol <antol.peshkov@gmail.com>
2014-02-08 20:26:30 +0400

    Вынесение полоски здоровья

---
+++ GameProject/Assets/Scripts/LifeObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeObject : MonoBehaviour {
+
+	public float health;
+
+	// Use this for initialization
+	void Start () {
+		this.health = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
