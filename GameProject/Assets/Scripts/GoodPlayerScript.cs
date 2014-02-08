54c2f08d156e8ef26dd7112501269ca418f2c058
JanaVPetrova <janavpetrova@gmail.com>
2014-02-08 17:49:04 +0400

    good player reacts at mouse enter

---
+++ GameProject/Assets/Scripts/GoodPlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoodPlayerScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseEnter () {
+		Debug.Log("xxx");
+		this.transform.FindChild("Good_head_pain").gameObject.SetActive(true);
+	}
+	
+	void OnMouseExit () {
+		this.transform.FindChild("Good_head_pain").gameObject.SetActive(false);
+	}
+}
