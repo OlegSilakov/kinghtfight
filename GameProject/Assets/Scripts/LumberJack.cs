c8dc8416a9a74203e0154bf4040497418a861bcb
Oleg Silakov <enegizer93@gmail.com>
2014-02-09 01:00:01 +0400

    Система очков для угрока

--- GameProject/Assets/Scripts/LumberJack.cs
+++ GameProject/Assets/Scripts/LumberJack.cs
@@ -8,7 +8,7 @@ public class LumberJack : MonoBehaviour {
 	public Texture cancel;
 
 	double workTimer;
-
+	PlayerProgress progress = new PlayerProgress();
 	// Use this for initialization
 	void Start () {
 		workTimer = myTimer;
@@ -25,13 +25,22 @@ public class LumberJack : MonoBehaviour {
 			workTimer -= Time.deltaTime;
 			GUI.Label (new Rect (Screen.width / 2 - 150, Screen.height / 2 - 150, 300, 300), ((int)workTimer).ToString ());
 		}
+		if (workTimer == 0) {
+			int playerPoints = progress.getPoints();
+			playerPoints += 10;
+		}
 		if (workTimer <= 0) {
-			GUI.Label (new Rect (Screen.width / 2 - 150, Screen.height / 2 - 150, 300, 300), "You finished chop-chop!");
+			int playerPoints = progress.getPoints();
+			GUI.Label (new Rect (Screen.width / 2 - 150, Screen.height / 2 - 150, 300, 300), "You finished chop-chop! You points: " + playerPoints.ToString());
 
 			if (GUI.Button(new Rect (Screen.width / 2 - 125, Screen.height / 2 + 40, 100, 100), reload)) {
 				workTimer = myTimer;
+				playerPoints += 10;
+				progress.savePoints(playerPoints);
 			}
 			if (GUI.Button(new Rect(Screen.width / 2 + 25, Screen.height / 2 + 40, 100, 100), cancel)) {
+				playerPoints += 10;
+				progress.savePoints(playerPoints);
 				Application.LoadLevel(0);
 			}
 		}
---
+++ GameProject/Assets/Scripts/PlayerProgress.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProgress : MonoBehaviour {
+
+	/*// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+
+	public void savePoints(int points) {
+		PlayerPrefs.SetInt ("player", points);
+	}
+
+	public int getPoints() {
+		if (!PlayerPrefs.HasKey ("player")) {
+			return 200;
+		} else {
+			return PlayerPrefs.GetInt("player");
+		}
+	}
+
+}
