8be1475b6a9737e3e333d77c1d8225bb5a135942
Oleg Silakov <enegizer93@gmail.com>
2014-02-08 15:20:40 +0400

    Сцена рубки леса

---
+++ GameProject/Assets/Scripts/LumberJack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LumberJack : MonoBehaviour {
+	public double myTimer = 10.0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myTimer > 0){
+			myTimer -= Time.deltaTime;
+		}
+		if(myTimer <= 0){
+			Debug.Log("GAME OVER");
+		}
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect (Screen.width / 2, Screen.height / 2, 300, 300), ((int)myTimer).ToString());
+	}
+
+	void chopWood() {
+	
+	}
+}
