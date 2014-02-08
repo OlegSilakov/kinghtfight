9ab4f316881a00e885ae70146d463f7007a37ce5
Oleg Silakov <enegizer93@gmail.com>
2014-02-08 16:12:07 +0400

    исправил ошибки с таймером

--- GameProject/Assets/Scripts/LumberJack.cs
+++ GameProject/Assets/Scripts/LumberJack.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class LumberJack : MonoBehaviour {
 	public double myTimer = 10.0;
+	public double workTimer;
 	public GUISkin mySkin;
 	// Use this for initialization
 	void Start () {
-	
+		workTimer = myTimer;
 	}
 	
 	// Update is called once per frame
@@ -16,12 +17,19 @@ public class LumberJack : MonoBehaviour {
 
 	void OnGUI() {
 		GUI.skin = mySkin;
-		if (myTimer > 0) {
-			myTimer -= Time.deltaTime;
-			GUI.Label (new Rect (Screen.width / 2, Screen.height / 2, 300, 300), ((int)myTimer).ToString ());
+		if (workTimer > 0) {
+			workTimer -= Time.deltaTime;
+			GUI.Label(new Rect (Screen.width / 2, Screen.height / 2 - 50, 300, 300), ((int)workTimer).ToString ());
 		}
-		if (myTimer <= 0) {
-			GUI.Label (new Rect (Screen.width / 2, Screen.height / 2, 300, 300), "Вы закончили рубуить лес!");
+		if (workTimer <= 0) {
+			GUI.Label(new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 300, 300), "Вы закончили рубить лес!");
+
+			if (GUI.Button(new Rect (Screen.width / 2 - 200, Screen.height / 2, 200, 100), "Заново")) {
+				workTimer = myTimer;
+			}
+			if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 200, 100), "Выйти")) {
+			
+			}
 		}
 	}
 
