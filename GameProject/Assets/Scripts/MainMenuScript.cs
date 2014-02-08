8b8d2dddbac06247de200a65800fc041e31cf5f0
VladShturma <uchiha248@mail.ru>
2014-02-08 19:41:56 +0400

    доделал главное меню

--- GameProject/Assets/Scripts/MainMenuScript.cs
+++ GameProject/Assets/Scripts/MainMenuScript.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class MainMenuScript : MonoBehaviour {
 
+	public GUISkin mySkin;
 	public Texture fightButton;
 	public Texture chopButton;
 	public Texture exitButton;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -18,6 +19,15 @@ public class MainMenuScript : MonoBehaviour {
 	}
 
 	void OnGUI() {
-
+		GUI.skin = mySkin;
+		if (GUI.Button(new Rect(Screen.width / 2 - 230, Screen.height / 2 - 40, 140, 80), fightButton)) {
+			Application.LoadLevel(1);
+		}
+		if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 40, 140, 80), chopButton)) {
+			Application.LoadLevel(2);
+		}
+		if (GUI.Button(new Rect(Screen.width / 2 + 90, Screen.height / 2 - 40, 140, 80), exitButton)) {
+			Application.Quit();
+		}
 	}
 }
