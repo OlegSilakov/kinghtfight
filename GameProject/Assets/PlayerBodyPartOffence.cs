f8ab96ee6474fab5a98eb381e02fedb61e9e1eab
Antol <antol.peshkov@gmail.com>
2014-02-08 18:53:26 +0400

    Выделение на втором игроке

---
+++ GameProject/Assets/PlayerBodyPartOffence.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBodyPartOffence : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUpAsButton () {
+		GameObject player = GameObject.Find("Enemy");
+		Offence offence = player.GetComponent("Offence") as Offence;
+		
+		offence.selectedBodyPart = this.gameObject;
+	}
+}
---
+++ GameProject/Assets/Scripts/Offence/Offence.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Offence : MonoBehaviour {
+
+	private GameObject _selectedBodyPart;
+	public GameObject selectedBodyPart {
+		set {
+			if (_selectedBodyPart) {
+				SpriteRenderer selectedRender = _selectedBodyPart.GetComponent("SpriteRenderer") as SpriteRenderer;
+				selectedRender.color = Color.white;
+			}
+			this._selectedBodyPart = value;
+			
+			SpriteRenderer render = this._selectedBodyPart.gameObject.GetComponent("SpriteRenderer") as SpriteRenderer;
+			render.color = Color.red;
+		}
+		get {
+			return this._selectedBodyPart;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
