64bfa15fef6b71a35770195da6f0c43ef63ea52a
JanaVPetrova <janavpetrova@gmail.com>
2014-02-09 01:06:32 +0400

    add offence and defence presence validation

--- GameProject/Assets/Scripts/RedButtonScript.cs
+++ GameProject/Assets/Scripts/RedButtonScript.cs
@@ -2,23 +2,39 @@
 using System.Collections;
 
 public class RedButtonScript : MonoBehaviour {
+	public GameObject player;
+	public GameObject enemy;
+
+	Defence defence;
+	Offence offence;
+
+	public Texture redButton;
+	public GUISkin mySkin;
+
+	private bool fightButtonEnabled = true;
 
 	// Use this for initialization
 	void Start () {
-	
+		Time.timeScale = 0;
+		offence = enemy.GetComponent<Offence> ();
+		defence = player.GetComponent<Defence> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-	void OnMouseDown () {
-		this.transform.localScale = new Vector3(0.9f, 0.9f, 1.0f);
 
 	}
 
-	void OnMouseUp () {
-		this.gameObject.SetActive(false);
+	void OnGUI() {
+		if (!fightButtonEnabled) {
+			return;
+		}
+		GUI.skin = mySkin;
+		if (defence.selectedBodyPart && offence.selectedBodyPart) {
+			if (GUI.Button(new Rect (Screen.width / 2 - 40, Screen.height / 2 - 40, 80, 80), redButton)) {
+				fightButtonEnabled = false;
+				Time.timeScale = 1;
+			}
+		}
 	}
 }
