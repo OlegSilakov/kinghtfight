fd1335da9f6ad8d8208874b0773dbfadc0604cd2
JanaVPetrova <janavpetrova@gmail.com>
2014-02-08 21:48:55 +0400

    big red button added

---
+++ GameProject/Assets/Scripts/RedButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		this.transform.localScale = new Vector3(0.9f, 0.9f, 1.0f);
+
+	}
+
+	void OnMouseUp () {
+		this.gameObject.SetActive(false);
+	}
+}
