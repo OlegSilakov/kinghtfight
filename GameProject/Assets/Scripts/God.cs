875db187d3caa4d36256dd80e40b7563079315c9
Antol <antol.peshkov@gmail.com>
2014-02-09 00:31:44 +0400

    Божественный объект

---
+++ GameProject/Assets/Scripts/God.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class God : MonoBehaviour {
+
+	private VirtualEnemy virtualEnemy;
+
+	// Use this for initialization
+	void Start () {
+		virtualEnemy = new VirtualEnemy ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Fight() {
+		string enemyAttack = virtualEnemy.getAttackPartOfBody ();
+		string enemyDefence = virtualEnemy.getDefencePartOfBody ();
+		string playerAttack = this.getAttackPartOfBody ();
+		string playerDefence = this.getDefencePartOfBody ();
+
+		this.startFightAnimation();
+	}
+
+	private string getAttackPartOfBody(){
+		return "legs";
+	}
+	
+	private string getDefencePartOfBody(){
+		return "head";
+	}
+
+	private void startFightAnimation(){
+		GameObject player = GameObject.Find("Player");
+		GameObject enemy = GameObject.Find("Enemy");
+
+		player.GetComponent<Animator>().SetTrigger("Walk");
+		enemy.GetComponent<Animator>().SetTrigger("Walk");
+
+	}
+}
