dc05300c379bbce1f401fcf25493569d2adb2420
Antol <antol.peshkov@gmail.com>
2014-02-08 16:31:26 +0400

    Реакция персоназа на наведение мыши, реакция в консоли

---
+++ GameProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseEnter () {
+		Debug.Log("Enter");
+	}
+
+	void OnMouseExit () {
+		Debug.Log("Exit");
+	}
+}
