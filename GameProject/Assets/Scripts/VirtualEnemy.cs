fe8e1a841c86942a1eb8655bf450d7818770a25a
Oleg Silakov <enegizer93@gmail.com>
2014-02-08 18:18:40 +0400

    Виртуальный противник

---
+++ GameProject/Assets/Scripts/VirtualEnemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirtualEnemy : MonoBehaviour {
+	private int health;
+	private ArrayList partsOfBody = new ArrayList() { "head", "body", "hands", "legs"};
+
+	/*// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+
+	public string getAttackPartOfBody(){
+		int val = (int)(Random.value * 4);
+		Debug.Log (val);
+		return partsOfBody[val].ToString();
+	}
+
+	public string getDefencePartOfBody(){
+		int val = (int)(Random.value * 4);
+		return partsOfBody[val].ToString();
+	}
+
+}
