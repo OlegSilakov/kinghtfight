e4b871c97783989b15b7607f8eed7fe98a3bf549
Oleg Silakov <enegizer93@gmail.com>
2014-02-09 06:30:01 +0400

    Меню

---
+++ GameProject/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+	public Texture pauseTexture;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 15, 0, Screen.width / 10, Screen.height / 8), pauseTexture)) {
+			Application.LoadLevel(0);
+		}
+	}
+}
