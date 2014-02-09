6f9504195d72ae84629dd27cd8ccf9651803432e
JanaVPetrova <janavpetrova@gmail.com>
2014-02-09 04:20:43 +0400

    add fight sound

---
+++ GameProject/Assets/Scripts/FightSoundScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightSoundScript : MonoBehaviour {
+	public AudioClip fightSound;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PlaySound() {
+		SoundPlayer player = SoundPlayer.getInstance ();
+		player.play (fightSound);	
+	}
+}
